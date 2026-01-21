50ca981a6e312090910214729d54a1496f4052b8
Anthonyo02 <hei.anthonyo.2@gmail.com>
2026-01-21 04:59:16 +0300

    vita build

--- components/ui/materielUtils.ts
+++ components/ui/materielUtils.ts
@@ -10,11 +10,11 @@ export const calculerDisponibiliteMateriel = (
     let enUtilisation = 0;
 
     projets.forEach((p) => {
-      const dateDebut = new Date(p.dateDebut);
-      const date_fin = p.date_fin ? new Date(p.date_fin) : new Date(p.dateDebut);
+      const date_debut = new Date(p.date_debut);
+      const date_fin = p.date_fin ? new Date(p.date_fin) : new Date(p.date_debut);
       const current = new Date(date);
 
-      if (current >= dateDebut && current <= date_fin) {
+      if (current >= date_debut && current <= date_fin) {
         const matProjet = p.materiel.find((pm) => pm.id === m.id);
         if (matProjet) {
           enUtilisation += matProjet.utiliser;
